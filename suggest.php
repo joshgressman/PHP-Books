68d40419fa9404e8263d604419bb2ca6af6e3fe0
joshgressman <joshgressman@gmail.com>
2016-11-29 14:54:56 -0600

    added new files

--- suggest.php
+++ suggest.php
@@ -1,13 +1,42 @@
 <?php $pageTitle = "Suggest a Media Item";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = $_POST["name"];
+  $email = $_POST["email"];
+  $details = $_POST["details"];
+
+  echo "<pre>";
+  $email_body = "";
+  $email_body .= "Name " . $name . "\n";
+  $email_body .= "Email ". $email . "\n";
+  $email_body .= "Details " . $details . "\n";
+  echo $email_body;
+  echo "</pre>";
+  //.= adds the value of new varible value to the old varibale concatinates one
+  //after the other. will return
+  //  Name Joshua Gressman
+  // Email joshgressman@gmail.com
+  // Details ferfe
+  header("location:suggest.php?status=thanks"); //will redirect to thanks message
+}
+
+
+//To do: send email
+
+$pageTitle = "Suggest a Media Item";
 $section = "suggest";
 include("include/header.php"); ?> <!--include header to header -->
 
 <div class="section page">
   <div class="wrapper">
     <h1>Suggest a Media Item</h1>
+    <?php if(isset($_GET["status"]) && $_GET["status"] == "thanks"){
+      echo "<p>
+        Thanks for the email! I&rsquo;ll check out your suggestion shortly!
+      </p>";
+    } else { ?>
     <P>If you think there is something I&rsquo;m missing, let me know! Complete the form to
     send me an email.</P>
-    <form method="post" action="process.php">  
+    <form method="post" action="suggest.php">
       <table>
         <tr>
           <th><label for="name">Name</label></th>
@@ -24,6 +53,7 @@ include("include/header.php"); ?> <!--include header to header -->
       </table>
       <input type="submit" value="send" />
     </form>
+    <?php } ?>
   </div>
 </div>
 
